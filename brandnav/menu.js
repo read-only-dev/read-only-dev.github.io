06faa3839801da319c56a4ef6a698f20de682c21
read-only-dev <113313926+read-only-dev@users.noreply.github.com>
2022-11-09 16:43:27 -0800

    navigation assignment with JS

---
+++ brandnav/menu.js
@@ -0,0 +1,9 @@
+function showmenu(){
+
+    document.getElementById('menu-overlay').style.display="block";
+}
+
+function hidemenu(){
+
+    document.getElementById('menu-overlay').style.display="none";
+}
